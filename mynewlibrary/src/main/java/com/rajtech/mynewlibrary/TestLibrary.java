f4bcdd3576056a659f27ff6b66ea39e845d63077
Raja kumaran <rajakumaransundararajan@gmail.com>
2021-01-10 12:15:11 +0530

    first commit

---
+++ mynewlibrary/src/main/java/com/rajtech/mynewlibrary/TestLibrary.java
@@ -0,0 +1,12 @@
+package com.rajtech.mynewlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class TestLibrary {
+    public static void s(Context c, String message){
+
+//        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+    }
+}
